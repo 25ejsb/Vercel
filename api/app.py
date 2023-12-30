c958338642e1c2c5c50385c8ab86b7f59179aee3
25ejsb <eitan.brochstein@icloud.com>
2023-12-30 15:04:24 -0500

    lol

--- api/app.py
+++ api/app.py
@@ -11,21 +11,22 @@ def get_db_client(host="mongodb+srv://eitanbrochstein:25Greenseed@cluster0.rhtkm
 @app.route("/")
 def home():
     # create one
-    try:
-        client = get_db_client()
-        db = client["Flask"]
-        db.users.insert_one({"username": "Yes"})
-        # update one
-        db.users.find_one_and_update({"username": "Yes"}, {"$set": {
-            "username": "YEs"
-        }})
-        # delete one
-        db.users.find_one_and_delete({"username": "YEs"})
-        print("Here")
-        return render_template("index.html")
-    except:
-        print("Here 2")
-        return render_template("index.html")
+    # try:
+    #     client = get_db_client()
+    #     db = client["Flask"]
+    #     db.users.insert_one({"username": "Yes"})
+    #     # update one
+    #     db.users.find_one_and_update({"username": "Yes"}, {"$set": {
+    #         "username": "YEs"
+    #     }})
+    #     # delete one
+    #     db.users.find_one_and_delete({"username": "YEs"})
+    #     print("Here")
+    #     return render_template("index.html")
+    # except:
+    #     print("Here 2")
+    #     return render_template("index.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
