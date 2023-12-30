3ab2acd5ddb771dfd082ef2fb5ce2694d9dc946c
25ejsb <eitan.brochstein@icloud.com>
2023-12-30 15:16:25 -0500

    lol

--- app.py
+++ app.py
@@ -6,29 +6,28 @@ Bootstrap(app)
 app.config["MONGO_URI"] = "mongodb+srv://eitanbrochstein:25Greenseed@cluster0.rhtkmvj.mongodb.net"
 mongo = PyMongo(app)
 
-# def get_db_client(host="mongodb+srv://eitanbrochstein:25Greenseed@cluster0.rhtkmvj.mongodb.net", port=27017):
-#         client = MongoClient(host=host)
-#         return client
+def get_db_client(host="mongodb+srv://eitanbrochstein:25Greenseed@cluster0.rhtkmvj.mongodb.net", port=27017):
+        client = MongoClient(host=host)
+        return client
 
 @app.get("/")
 def home():
-    # create one
-    # try:
-    #     client = get_db_client()
-    #     db = client["Flask"]
-    #     db.users.insert_one({"username": "Yes"})
-    #     # update one
-    #     db.users.find_one_and_update({"username": "Yes"}, {"$set": {
-    #         "username": "YEs"
-    #     }})
-    #     # delete one
-    #     db.users.find_one_and_delete({"username": "YEs"})
-    #     print("Here")
-    #     return render_template("index.html")
-    # except:
-    #     print("Here 2")
-    #     return render_template("index.html")
-    return render_template("index.html")
+    #create one
+    try:
+        client = get_db_client()
+        db = client["Flask"]
+        db.users.insert_one({"username": "Yes"})
+        # update one
+        db.users.find_one_and_update({"username": "Yes"}, {"$set": {
+            "username": "YEs"
+        }})
+        # delete one
+        db.users.find_one_and_delete({"username": "YEs"})
+        print("Here")
+        return render_template("index.html")
+    except:
+        print("Here 2")
+        return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
