f647b93da951037224035e1b9a7b85823346c77e
25ejsb <eitan.brochstein@icloud.com>
2023-12-29 22:42:48 -0500

    lol

--- api/app.py
+++ api/app.py
@@ -1,20 +1,31 @@
 from flask import Flask, render_template
-from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo, MongoClient
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://eitanbrochstein:25Greenseed@cluster0.rhtkmvj.mongodb.net/Flask"
+app.config["MONGO_URI"] = "mongodb+srv://eitanbrochstein:25Greenseed@cluster0.rhtkmvj.mongodb.net"
 mongo = PyMongo(app)
+
+def get_db_client(host="mongodb+srv://eitanbrochstein:25Greenseed@cluster0.rhtkmvj.mongodb.net", port=27017):
+        client = MongoClient(host=host)
+        return client
 
 @app.route("/")
 def home():
     # create one
-    mongo.db.users.insert_one({"username": "Yes"})
-    # update one
-    mongo.db.users.find_one_and_update({"username": "Yes"}, {"$set": {
-        "username": "YEs"
-    }})
-    # delete one
-    mongo.db.users.find_one_and_delete({"username": "YEs"})
-    return render_template("index.html")
+    try:
+        client = get_db_client()
+        db = client["Flask"]
+        db.users.insert_one({"username": "Yes"})
+        # update one
+        db.users.find_one_and_update({"username": "Yes"}, {"$set": {
+            "username": "YEs"
+        }})
+        # delete one
+        db.users.find_one_and_delete({"username": "YEs"})
+        print("Here")
+        return render_template("index.html")
+    except:
+        print("Here 2")
+        return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
