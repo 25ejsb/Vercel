ea0be265385a40d5e870e29e29a9cea31cb819d4
25ejsb <eitan.brochstein@icloud.com>
2024-01-01 11:35:29 -0500

    lol

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
-import pymongo
+import pymongo, certifi, ssl
+ca = certifi.where()
 from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 Bootstrap(app)
-mongo_url = "mongodb+srv://eitanbrochstein:25Greenseed@cluster0.rhtkmvj.mongodb.net/Flask?retryWrites=true&w=majority&ssl=true&tlsCAFile=isrgrootx1.pem"
-client = pymongo.MongoClient(mongo_url)
+mongo_url = "mongodb+srv://eitanbrochstein:25Greenseed@cluster0.rhtkmvj.mongodb.net/Flask?retryWrites=true&w=majority"
+client = pymongo.MongoClient(mongo_url, tls=True, tlsAllowInvalidCertificates=True, tlsCAFile=certifi.where())
 db = client["Flask"]
 
 import dns.resolver
@@ -18,4 +19,4 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True)
